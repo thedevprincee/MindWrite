53d623ee9b7c2077079337e558100ad80f2a0414
Valerie17-tech <valeriegeorge1711@gmail.com>
2023-02-16 23:24:37 +0100

    n

--- src/components/molecules/notecard/Notecard.jsx
+++ src/components/molecules/notecard/Notecard.jsx
@@ -5,26 +5,26 @@ import './notecard.css'
 
 const Notecard = () => {
   return (
-    <div className='card'>
-        <div>
+    <div className='card' id='container'>
+        <div className='sec1'>
             <div className='bell'>
-                <Bell />
+             <Bell className='notification-icon' />
             </div>
-            <div>
+                <div className='details'>
                     <div className='title'>
                       <Text>Summer Vacation</Text>
                     </div>
-            <div className=''>
-              <Text>16 Feb 2023, 16:35 PM</Text>
-            </div>
-            </div> 
-        </div>
-     <div>
-        <Text>Image</Text>
-     </div>
-     <div>
-     <Text>Je m'appelle George Valérie, aujourd'hui j'ai eu un cours à Gomycode.</Text>
-     </div>
+                  <div className='date'>
+                    <Text>16 Feb 2023, 16:35 PM</Text>
+                  </div>
+              </div> 
+             </div>
+           <div className='img'>
+         <Text>Image</Text>
+         </div>
+           <div className='notes'>
+          <Text>Je m'appelle George Valérie, aujourd'hui j'ai eu un cours à Gomycode.</Text>
+         </div>
     </div>
   )
 }

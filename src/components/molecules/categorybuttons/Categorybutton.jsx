481c06124f54672613534885b2e876ed081a3d44
DIRITEC <edwin.ediri@gmail.com>
2023-02-16 18:22:24 -0800

    first commit

---
+++ src/components/molecules/categorybuttons/Categorybutton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Categorybutton = () => {
+  return (
+    <div>
+      <button class="button All Notes">white</button>
+      <button class="button Reminder">White</button>
+      <button class="button Audio">White</button>
+      <button class="button Images">White</button>
+    </div>
+  )
+}
+
+export default Categorybutton

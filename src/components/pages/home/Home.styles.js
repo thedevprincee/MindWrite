7530379c3cf627313858eb9fc050daa7bac3bb74
thedevprincee <thedevprince@gmail.com>
2023-02-17 01:31:01 -0800

    Page split

--- src/components/pages/home/Home.styles.js
+++ src/components/pages/home/Home.styles.js
@@ -1,7 +1,46 @@
 import styled from 'styled-components'
 
-const HomeWrapper = styled.div`
-    border: 1px solid red;
+export const HomeWrapper = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0; 
+    overflow: hidden;
 `
-
-export default HomeWrapper
+export const AppDisplay = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0; 
+    overflow-y: auto;
+    overflow-x: hidden;
+    @media screen and (min-width:590px){
+        border: 2px solid black;
+        width: 30%;
+        min-width: 300px;
+    }
+`
+export const WebDisplay = styled.div`
+    display: none;
+    @media screen and (min-width:590px){
+        border: 2px solid black;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        margin: 0;
+        padding: 0; 
+        overflow-y: auto;
+        overflow-x: hidden;
+        width: 70%;
+        min-width: 300px;
+        height: 100vh;
+    }
+`
